0455d540b4e585d640429c2bf5a4348a0a06d176
Júlio César <julio.cesar@myorgtech.com>
2026-07-20 16:46:35 -0300

    aulas sobre array e desafio do pensionato

---
+++ comportamento-de-memoria-arrays-listas/src/array/pensionato/Main.java
@@ -0,0 +1,40 @@
+package array.pensionato;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int quantityRoom = sc.nextInt();
+        sc.nextLine();
+
+        Student[] rooms = new Student[9];
+
+        for(int i = 0; i < quantityRoom; i++){
+            System.out.println("\nRent #" + (i + 1));
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            sc.nextLine();
+
+            rooms[room] = new Student(name, email, room);
+        }
+
+        System.out.println("\nBusy rooms:");
+        for(int i = 0; i < rooms.length; i++){
+            if(rooms[i] != null)
+                System.out.println(i + ": " + rooms[i].getName() + ", " + rooms[i].getEmail());
+        }
+    }
+}
---
+++ comportamento-de-memoria-arrays-listas/src/array/vetores.md
@@ -0,0 +1,8 @@
+# Array
+    * Manipulação de vetor de elementos tipo valor (primitivo)  
+    * Manipulação de vetor de elementos tipo referência (classe)
+
+    Array é uma estrutura de dados do mesmo tipo, ordenada e alocadas de uma vez só
+    
+    Vantagens: Acesso imediato aos elementos pela sua posição
+    Desvantagens: Tamanho fixo e dificuldade para se realizar inserções e deleções
