78189403ef5e4d1bdb9829f1559e263714199657
Júlio César <juliocesar.gm6000@gmail.com>
2026-08-05 17:46:35 -0300

    Aulas de JDBC, configuracao, instalacao, conexao, etc.

---
+++ acesso-ao-banco-jdbc/src/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import db.DB;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main (String[] args){
+
+        Connection conn = DB.getConnection();
+
+        DB.closeConnection();
+
+    }
+}
