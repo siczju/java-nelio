3d0fbd99c7001a6c8f7852415ce8895e8e3a711d
Júlio César <juliocesar.gm6000@gmail.com>
2026-08-06 10:57:47 -0300

    Atualização de dados no banco com JDBC

--- acesso-ao-banco-jdbc/src/aplicacao/Main.java
+++ acesso-ao-banco-jdbc/src/aplicacao/Main.java
@@ -10,47 +10,30 @@ public class Main {
 
         Connection conn = null;
         PreparedStatement st = null;
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         try{
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(name, email, birthdate, basesalary, departmentid)"
-                    + "VALUES "
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS // para retornar a chave primaria criada
+                    "UPDATE seller "
+                    + "SET basesalary = basesalary + ? " // para incrementar
+                    + "WHERE "
+                    + "(departmentid = ?)"
             );
 
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, java.sql.Date.valueOf(LocalDate.parse("22/04/1985", dtf)));
-            st.setDouble(4, 3000);
-            st.setInt(5, 4);
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
 
-            int rowsAffected = st.executeUpdate(); // qnts linhas foram alteradas no banco
+            int rows = st.executeUpdate();
 
-            if(rowsAffected > 0){
-                ResultSet rs = st.getGeneratedKeys(); // chave primaria vai pro ResultSet, com um ou mais chaves
-                while(rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! ID: " + id);
-                }
-
-
-            }
-            else{
-                System.out.println("No Rows Affected");
-            }
-
+            System.out.println("Rows affected: " + rows);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
         finally{
             DB.closeStatement(st);
-            DB.closeConnection(); // sempre fecha a conexão por ultimo!
+            DB.closeConnection();
         }
 
     }
