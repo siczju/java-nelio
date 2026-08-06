14f12f5567ee8bbbd32afd2e03f0c1f91954ef6e
Júlio César <juliocesar.gm6000@gmail.com>
2026-08-06 16:58:19 -0300

    Implementação de atualização de dados do seller no banco com JDBC

--- dao-jdbc/src/model/impl/SellerDaoJDBC.java
+++ dao-jdbc/src/model/impl/SellerDaoJDBC.java
@@ -62,7 +62,28 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                "UPDATE seller "
+                    + "SET name = ?, email = ?, birthdate = ?, basesalary = ?, departmentid = ? "
+                    + "WHERE id = ?"
+            );
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, Date.valueOf(obj.getBirthdate()));
+            st.setDouble(4, obj.getBasesalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
 
+            st.executeUpdate();
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally{
+            DB.closeStatement(st);
+        }
     }
 
     @Override
